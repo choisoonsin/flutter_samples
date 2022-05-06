a0e99824af4cecff1ff633739f8c752fdebfc807
Choi young hoon <cyh0214@namuplanet.com>
2022-05-06 16:21:15 +0900

    init

---
+++ exam_bloc_timer_new/lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: _Timer(),
+    );
+  }
+}
+
+class _Timer extends StatelessWidget {
+  const _Timer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        alignment: Alignment.center,
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.topRight,
+                    end: Alignment.bottomLeft,
+                    colors: [Colors.blue, Colors.red])),
+          ),
+        ],
+      ),
+    );
+  }
+}
